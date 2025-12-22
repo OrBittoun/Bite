169a1f7a4351cb7450211e5029c6b4e942539457
Or Bitton <orbitzx25@gmail.com>
2025-12-22 12:37:06 +0200

    added pics & button into cards

--- app/src/main/java/com/example/first_app_version/ui/all_kitchens/HomePageFragment.kt
+++ app/src/main/java/com/example/first_app_version/ui/all_kitchens/HomePageFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -13,6 +14,7 @@ import com.example.first_app_version.databinding.HomePageLayoutBinding
 import com.example.first_app_version.ui.KitchenViewModel
 import com.example.first_app_version.ui.HomeCategoriesAdapter
 import com.example.first_app_version.ui.HomeCategory
+import com.google.android.material.appbar.AppBarLayout
 
 class HomePageFragment : Fragment() {
 
@@ -21,17 +23,21 @@ class HomePageFragment : Fragment() {
 
     private val viewModel: KitchenViewModel by activityViewModels()
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = HomePageLayoutBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         val categories = listOf(
             HomeCategory(
